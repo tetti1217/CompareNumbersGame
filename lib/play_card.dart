cdaebfe8917a92d7ada327cc9e14abe9250b292a
木戸俊輔 <kidokidofire@kidokido-mac.local>
2024-04-15 22:23:28 +0900

    Implement player/computer card class.

--- lib/play_card.dart
+++ lib/play_card.dart
@@ -2,19 +2,42 @@ import 'package:flutter/material.dart';
 
 class PlayCard extends StatefulWidget {
   final Image image;
-  final Alignment initialAlignment;
   
   const PlayCard({
     super.key,
     required this.image,
-    required this.initialAlignment,
   });
 
   @override
   PlayCardState createState() => PlayCardState();
 }
 
-class PlayCardState extends State<PlayCard> {
+class PlayCardState<T extends PlayCard> extends State<T> {
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 60,
+      height: 100,
+      child: widget.image,
+    );
+  }
+}
+
+class PlayerCard extends PlayCard {
+  final Alignment initialAlignment;
+  
+  const PlayerCard({
+    super.key,
+    required super.image,
+    required this.initialAlignment,
+  });
+
+  @override
+  PlayerCardState createState() => PlayerCardState();
+}
+
+class PlayerCardState extends PlayCardState<PlayerCard> {
   late Alignment _alignment;
 
   @override
@@ -25,7 +48,7 @@ class PlayCardState extends State<PlayCard> {
 
   void _onTap() {
     setState(() {
-      _alignment = Alignment.center;
+      _alignment = const Alignment(0.0, 0.3);
     });
   }
 
@@ -37,11 +60,49 @@ class PlayCardState extends State<PlayCard> {
       duration: const Duration(seconds: 1),
       child: GestureDetector(
         onTap: _onTap,
-        child: SizedBox(
-          width: 60,
-          height: 100,
-          child: widget.image,
-        ),
+        child: super.build(context),
+      ),
+    );
+  }
+}
+
+class ComputerCard extends PlayCard {
+  final Alignment initialAlignment;
+  
+  const ComputerCard({
+    super.key,
+    required super.image,
+    required this.initialAlignment,
+  });
+
+  @override
+  ComputerCardState createState() => ComputerCardState();
+}
+
+class ComputerCardState extends PlayCardState<ComputerCard> {
+  late Alignment _alignment;
+
+  @override
+  void initState() {
+    super.initState();
+    _alignment = widget.initialAlignment;
+  }
+
+  void _onTap() {
+    setState(() {
+      _alignment = const Alignment(0.0, -0.3);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedAlign(
+      alignment: _alignment,
+      curve: Curves.ease,
+      duration: const Duration(seconds: 1),
+      child: GestureDetector(
+        onTap: _onTap,
+        child: super.build(context),
       ),
     );
   }
